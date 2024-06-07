b0cd912ba52360d2a6d3090e8cfe3fccf05f4bcb
DanteWolfXz <nacho17tm10@gmail.com>
2024-06-07 14:57:48 -0300

    asdas

--- CIENFUEGOS2/perfilusuario.js
+++ CIENFUEGOS2/perfilusuario.js
@@ -9,19 +9,25 @@ document.addEventListener('DOMContentLoaded', async () => {
       });
       if (response.ok) {
         const { user, orders } = await response.json();
-        // Actualizar el contenido del HTML con los datos del usuario
         document.getElementById('username').textContent = user.username;
         document.getElementById('email').textContent = user.email;
         document.getElementById('dni').textContent = user.dni;
         document.getElementById('phone').textContent = user.phone;
-        document.getElementById('isAdmin').textContent = user.isAdmin ? 'Sí' : 'No';
-        // Mostrar los pedidos del usuario
-        const orderList = document.getElementById('order-list');
-        orders.forEach(order => {
+        // Mostrar los pedidos del usuario si existen
+        if (orders && orders.length > 0) {
+          const orderList = document.getElementById('order-list');
+          orders.forEach(order => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `Pedido #${order._id}: ${order.quantity} x ${order.product}`;
+            orderList.appendChild(listItem);
+          });
+        } else {
+          // Si no hay pedidos, muestra un mensaje indicando que no hay pedidos disponibles
+          const orderList = document.getElementById('order-list');
           const listItem = document.createElement('li');
-          listItem.textContent = `Pedido #${order._id}: ${order.quantity} x ${order.product}`;
+          listItem.textContent = 'No hay pedidos disponibles';
           orderList.appendChild(listItem);
-        });
+        }
       } else {
         console.error('Error al obtener los datos del usuario y sus pedidos:', response.statusText);
       }
