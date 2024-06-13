a9ac7a62f1b151cc799f7f8be0e6e28bb38163fa
DanteWolfXz <nacho17tm10@gmail.com>
2024-06-13 15:08:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -110,63 +110,6 @@
   const preferences = {}; // Objeto en memoria para almacenar las preferencias
 
   // Ruta para manejar la creación de preferencias de pago
-  app.post('/create_preference', async (req, res) => {
-    try {
-        const idempotencyKey = req.headers['x-idempotency-key'];
-        const orderDataList = req.body;
-  
-        console.log('Received orderDataList:', orderDataList);
-  
-        // Extraer el userId de orderDataList
-        const userIdObject = orderDataList.find(item => item.userId);
-        const userId = userIdObject ? userIdObject.userId : 'unknown_user';
-  
-        console.log('Extracted userId:', userId);
-  
-        // Filtrar orderDataList para eliminar el objeto userId
-        const items = orderDataList.filter(item => !item.userId).map(orderData => ({
-            title: orderData.title,
-            unit_price: Number(orderData.price),
-            quantity: Number(orderData.quantity),
-            currency_id: 'ARS',
-        }));
-  
-        console.log('Filtered items for preferenceData:', items);
-  
-        const preferenceData = {
-            items: items,
-            back_urls: {
-                success: 'https://ecoomerce-api-v7wq.onrender.com/pago-confirmado',
-                failure: 'https://ecoomerce-api-v7wq.onrender.com/pago-denegado',
-                pending: 'https://ecoomerce-api-v7wq.onrender.com/pago-pendiente',
-            },
-            auto_return: 'approved',
-            notification_url: 'https://ecoomerce-api-v7wq.onrender.com/webhook',
-        };
-  
-        console.log('Preference data to be sent to MercadoPago:', preferenceData);
-  
-        const preference = new Preference(client);
-        const result = await preference.create({ body: preferenceData, idempotencyKey });
-  
-        console.log('Preference creation result:', result);
-  
-        // Guardar userId asociado con preferenceId
-        preferences[result.id] = userId;
-  
-        res.json({
-            id: result.id,
-            init_point: result.init_point,
-            userId: userId, // Devolver el userId en la respuesta
-        });
-    } catch (error) {
-        console.error('Error creating preference:', error);
-        res.status(500).json({
-            error: 'Error al crear la preferencia :(',
-        });
-    }
-  });
-  
   const createOrder = async (orderData) => {
     try {
       console.log('Creating order with data:', orderData);
@@ -203,6 +146,7 @@
   
       if (body.topic === 'payment' && body.data && body.data.id) {
         const paymentId = body.data.id;
+        console.log('Fetching payment details for paymentId:', paymentId);
   
         // Fetch payment details
         const paymentResponse = await fetch(`https://api.mercadolibre.com/collections/notifications/${paymentId}`, {
@@ -224,6 +168,7 @@
         // Check if payment is approved
         if (paymentData.collection.status === 'approved') {
           const merchantOrderId = paymentData.collection.merchant_order_id;
+          console.log('Fetching merchant order details for merchantOrderId:', merchantOrderId);
   
           // Fetch merchant order details
           const orderResponse = await fetch(`https://api.mercadolibre.com/merchant_orders/${merchantOrderId}`, {
@@ -243,26 +188,20 @@
           console.log('Merchant order data received:', orderData);
   
           const products = orderData.items.map(item => ({
-            title: item.title,
-            quantity: item.quantity,
-            price: item.unit_price
+            productId: item.id, // assuming you need productId from order data
+            quantity: item.quantity
           }));
-  
-          console.log('Products extracted from merchant order data:', products);
   
           const userId = preferences[orderData.preference_id]; // Obtener userId usando preferenceId
           console.log('UserId associated with preferenceId:', userId);
   
           const order = {
             userId: userId,
-            preferenceId: orderData.preference_id,
-            merchantOrderId: orderData.id,
-            status: 'approved', // Orden aprobada
-            totalAmount: orderData.total_amount,
             products: products,
-            payer: {
-              email: orderData.payer.email
-            }
+            amount: orderData.total_amount,
+            address: orderData.shipping.address,
+            status: 'approved', // Orden aprobada
+            delivered: false // Assuming you want to initialize it as false
           };
   
           console.log('Order data to be sent to createOrder:', order);
@@ -274,6 +213,49 @@
           console.log('Payment status not approved:', paymentData.collection.status);
           res.sendStatus(200);
         }
+      } else if (body.topic === 'merchant_order' && body.resource) {
+        const merchantOrderId = body.resource.split('/').pop();
+        console.log('Fetching merchant order details for merchantOrderId:', merchantOrderId);
+  
+        // Fetch merchant order details
+        const orderResponse = await fetch(`https://api.mercadolibre.com/merchant_orders/${merchantOrderId}`, {
+          method: 'GET',
+          headers: {
+            Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}`
+          }
+        });
+  
+        if (!orderResponse.ok) {
+          const errorText = await orderResponse.text();
+          console.error('Error fetching merchant order:', errorText);
+          return res.status(500).json({ error: 'Error fetching merchant order.' });
+        }
+  
+        const orderData = await orderResponse.json();
+        console.log('Merchant order data received:', orderData);
+  
+        const products = orderData.items.map(item => ({
+          productId: item.id, // assuming you need productId from order data
+          quantity: item.quantity
+        }));
+  
+        const userId = preferences[orderData.preference_id]; // Obtener userId usando preferenceId
+        console.log('UserId associated with preferenceId:', userId);
+  
+        const order = {
+          userId: userId,
+          products: products,
+          amount: orderData.total_amount,
+          address: orderData.shipping.address,
+          status: 'approved', // Orden aprobada
+          delivered: false // Assuming you want to initialize it as false
+        };
+  
+        console.log('Order data to be sent to createOrder:', order);
+  
+        const createdOrder = await createOrder(order);
+        console.log('Order created successfully:', createdOrder);
+        res.sendStatus(200);
       } else {
         console.error('Invalid resource URL or unsupported topic:', body.resource, body.topic);
         res.status(400).json({ error: 'Invalid resource URL or unsupported topic' });
@@ -283,9 +265,6 @@
       res.sendStatus(500);
     }
   });
-  
-
-
 
 
   app.listen(process.env.PORT || 8000, () => {
